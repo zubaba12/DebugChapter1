a948490f4f747f8fd8a106ca81c1c7cf87df9e95
Zubaba Ahmed <zubabaahmed@Zubabas-MacBook-Air-2.local>
2022-11-10 15:13:37 -0600

    first commit

---
+++ src/DebugOne3.java
@@ -0,0 +1,9 @@
+public class DebugOne3{
+    public static void main(String[] args) {
+//        System.out.println("Over the river");
+//        System.out.println("and through the woods");
+//        System.out.println("to Grandmother's house we go");
+        System.out.println("Over the river " + "\nand through the woods " + "\nto Grandmother's house we go");
+    }
+    }
+
